b655507b31b0cba6e8b4848c10daf5ce3719818e
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-10-12 13:59:45 -0400

    Allow continuation if paused

--- scripts/confirm_200.py
+++ scripts/confirm_200.py
@@ -6,8 +6,9 @@ from rich import print
 
 console = Console()
 
-# Select all the fields with links
-fields_with_links = {
+STATUS_CODE_JSON = 'scripts/status-codes.json'
+OVERWRITE_JSON = False
+FIELDS_WITH_LINKS = {
     "annotations.csv": [
         'id', 'book_id', 'annotation_region', 'page_iiif'
     ],
@@ -22,16 +23,10 @@ fields_with_links = {
     ]
 }
 
-# TEST: Confirm this is all of the fields with links:
-for file, field_list in fields_with_links.items():
-    df = pd.read_csv(file)
-    for col in df.columns:
-        if df[col].astype(str).str.contains('http').any() and col not in field_list:
-            print(f'WARNING. Missing field in {file}: {col}')
 
 # Get unique links from all fields
 all_links = set()
-for file, field_list in fields_with_links.items():
+for file, field_list in FIELDS_WITH_LINKS.items():
     df = pd.read_csv(file)
     for col in field_list:
         for links in df[col].dropna().unique():
@@ -42,19 +37,40 @@ for file, field_list in fields_with_links.items():
 # TEST: Ensure all multivalued links are correctly parsed
 assert not any([';' in link for link in all_links])
 
-# Print summary
 print(f'Number of unique links: {len(all_links)}')
 
-# Find status code for all links
-url_status_codes = {}
-for i, link in enumerate(list(all_links)):
-    code = requests.get(link).status_code
-    url_status_codes[link] = code
-    if code == 200:
-        print(f'[green]\t{i}\t{code}\t{link}[/green]')
-    else:
-        print(f'[red]\t{i}\t{code}\t{link}[/red]')
 
+# DETERMINE STATUS CODES
+
+# Decide whether to continue where we left off or run script fresh
+if OVERWRITE_JSON:
+    url_status_codes = {}
+else:
+    with open(STATUS_CODE_JSON) as f:
+        url_status_codes = json.load(f)
+
+# Find status code for all links
+for i, link in enumerate(list(all_links)):
+    if link not in url_status_codes:
+        code = requests.get(link).status_code
+        url_status_codes[link] = code
+
+        if code == 200:
+            print(f'[green]\t{i}\t{code}\t{link}[/green]')
+        else:
+            print(f'[red]\t{i}\t{code}\t{link}[/red]')
+
+    # Update status code JSON periodically
     if i % 100 == 0:
-        with open('scripts/status-codes.json', 'w') as f:
+        with open(STATUS_CODE_JSON, 'w') as f:
             json.dump(url_status_codes, f, indent=4)
+
+# Write final status code JSON
+with open(STATUS_CODE_JSON, 'w') as f:
+    json.dump(url_status_codes, f, indent=4)
+
+# Print results summary
+print('Error URLs:')
+for url, status_code in url_status_codes.items():
+    if status_code != 200:
+        print(f'\t{url}')
